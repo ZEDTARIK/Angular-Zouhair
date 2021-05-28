91c65ccb06d215d69358a49ff5aa8990ac488ddc
ETTARAK <ettarak.zouhair@gmail.com>
2021-05-28 10:11:01 +0100

    Coding, Routting and call them into component parent

--- Angular-project/src/app/app-routing.module.ts
+++ Angular-project/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { ListEmployeeComponent } from './employee/list-employee.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+
+const appRouter: Routes = [
+  { path: 'employee-list', component: ListEmployeeComponent},
+  { path: 'employee-create', component: CreateEmployeeComponent},
+  { path: '', redirectTo: '/employee-list', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(appRouter)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
